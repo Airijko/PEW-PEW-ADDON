ae222f0ec498191f2451aa16e29b130e7d40817d
jojoj <jojojimenajr@gmail.com>
2026-03-25 13:30:22 -0500

    Added MPL License

--- src/main/java/com/airijko/endlessleveling/EndlessLevelingAddon.java
+++ src/main/java/com/airijko/endlessleveling/EndlessLevelingAddon.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2026 Airijko
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as defined by the Mozilla Public License, v. 2.0.
+ */
+
 package com.airijko.endlessleveling;
 
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
